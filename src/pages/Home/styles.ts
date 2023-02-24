53c89ac82ce51f6f22cc46c2e845f27cdd1c48dc
Rui Costa <47061282+ruiamcosta@users.noreply.github.com>
2023-02-24 11:28:59 +0000

    first commit

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const BaseCountdownButton = styled.button`
+  width: 100%;
+  padding: 1rem;
+  color: ${(props) => props.theme['gray-100']};
+  border: 0;
+  border-radius: 8px;
+  font-weight: bold;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${(props) => props.theme['green-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
+  }
+`
